d360e85cbf8f2531ec89b45a52e19dc3e01f4c2c
Aniruddha1999 <58836471+Aniruddha1999@users.noreply.github.com>
2020-10-01 13:08:55 +0530

    Revert "app/src/androidTest/java/com/example/myapp/ExampleInstrumentedTest.kt"

--- app/src/androidTest/java/com/example/myapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/myapp/ExampleInstrumentedTest.kt
@@ -16,9 +16,6 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    
-    
-    
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
